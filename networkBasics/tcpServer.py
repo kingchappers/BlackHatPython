9b4c00b6a7c721404818cd4f211a2cd2109eaa3d
Sam Chapman <sam.chapman@outlook.com>
2020-11-08 11:41:02 +0000

    updated with comments for understanding

--- networkBasics/tcpServer.py
+++ networkBasics/tcpServer.py
@@ -8,11 +8,12 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind((bind_ip,bind_port))
 
+# setting maximum backlog of connections to 5
 server.listen(5)
 
 print("[*] Listening on %s:%d" % (bind_ip, bind_port))
 
-# this is the client handling thread
+# this is the client handling thread and performs the recv and sends a message back to the client
 def handle_client(client_socket):
     # print out whet the client sends
     request = client_socket.recv(1024)
@@ -25,10 +26,11 @@ def handle_client(client_socket):
     client_socket.close()
 
 while True:
+    # receive client socket into client variable and remote connection details into addr variable
     client,addr = server.accept()
 
     print("[*] Accepted connection from: %s:%d" % (addr[0],addr[1]))
 
-    # spin up the client thread to handle incoming data
+    # spin up the client thread to handle incoming data and pass it the client object
     client_handler = threading.Thread(target=handle_client,args=(client,))
     client_handler.start()
