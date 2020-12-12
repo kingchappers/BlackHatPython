9edd550707f24b0cad6fc58ce76544a103520d70
Sam  Chapman <sam.chapman@outlook.com>
2020-12-12 19:31:42 +0000

    Added ssh server/client

---
+++ networkBasics/bh_sshcmd.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import threading
+import paramiko
+import subprocess
+
+def ssh_command(ip, user, passwd, command):
+    client = paramiko.SSHClient()
+    #Below line shows how to use ssh keys instead of username or password
+    #client.load_host_keys('/home/sam/.ssh/known_hosts')
+
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(ip, username=user, password=passwd)
+    ssh_session = client.get_transport().open_session()
+
+    if ssh_session.active:
+        ssh_session.exec_command(command)
+        print(ssh_session.recv(1024))
+
+    return
