600254e8627b1c1afa549ae3024657adf6e83d08
Sam  Chapman <sam.chapman@outlook.com>
2021-03-13 10:26:27 +0000

    get request no working

--- webHackery/getRequest.py
+++ webHackery/getRequest.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
-import urllib
+import urllib.request
 
-body = urllib.request.urlopen('https://samchapman.dev')
+body = urllib.request.urlopen("http://samchapman.dev")
+
+print(body.read)
