21f5f2e7de0c72a30f15a9097a070f4bad6db43f
Sam  Chapman <sam.chapman@outlook.com>
2021-03-20 17:09:59 +0000

    removed githubCommandAndControl as it uses plaintext username and passwords to interact with Github

--- githubCommanControl/trojan/modules/environment.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-#This retrieves environment variables set on the machine it runs on
-
-import os
-
-def run(**args):
-    print("[*] In environment module.")
-    return str(os.environ)
